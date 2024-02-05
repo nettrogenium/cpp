b3288b7469bde093269b39cc5dd37cd25087da0f
Nettwork <151877350+nettrogenium@users.noreply.github.com>
2024-02-05 16:19:20 +0300

    Update and rename rec_fibonacci to rec_fibonacci.cpp

---
+++ rec_fibonacci.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int fib(int n)
+{
+    if (n==0 || n==1 || n==2)
+        return 1;
+    return fib(n-1)+fib(n-2);
+}
+
+int main()
+{
+    int n; std::cin>>n;
+    std::cout<<fib(n);
+    return 0;
+}
